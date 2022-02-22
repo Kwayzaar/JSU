c2a6453104514b45da6e487a7e111d39f76f0fd8
eric <lee.eric.03@gmail.com>
2022-02-21 19:14:23 -0700

    finished 12

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+// VALUES AND VARIABLES 
+let country = "United States of America"
+let continent = "North America"
+let population = 329000000
+
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+
+// DATA TYPES
+let isIsland = false 
+let language 
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,32 @@
-let js = "amazing";
-console.log(40+8+23-10)
+// let js = "amazing";
+// console.log(40+8+23-10)
+
+// let firstName = "Matilda"
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
+
+// DYNAMIC TYPING + DATA TYPES
+true;
+let javascriptIsFun = true 
+console.log(javascriptIsFun)
+
+// console.log(typeof true)
+console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof "Eric")
+
+// assigns new value to variable, do not use "let"
+javascriptIsFun = "YES!"
+console.log(typeof javascriptIsFun)
+
+// undefined variables
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991 
+console.log(typeof year);
+
+console.log(typeof null)
 
