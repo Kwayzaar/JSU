768562297af70e1601bac99bf55e02436f508e6b
eric <lee.eric.03@gmail.com>
2022-02-28 11:44:56 -0700

    switch statements

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -20,7 +20,7 @@ console.log(typeof language)
 */
 
 // let, const, var
-language = "English"
+language = "english"
 // isIsland = true
 
 // Basic operators 
@@ -76,3 +76,32 @@ if (height){
 // } else {
 //   console.log('No borders')
 // }
+
+// Switch Statements 
+switch (language) {
+  
+  case 'chinese':
+    console.log('MOST number of native speakers')
+    break
+  
+    case 'spanish': 
+      console.log('2nd place in number of native speakers')
+      break
+  
+    case 'english': 
+      console.log('3rd place')
+      break
+  
+    case 'hindi': 
+      console.log('Number 4')
+      break 
+  
+    case 'arabic': 
+      console.log('5th most spoken language')
+      break
+  
+    default:
+    console.log('Great language too :D')
+}
+
+
