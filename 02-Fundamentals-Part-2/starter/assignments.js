9adaa6d519d71757cbc2eb64e741053a7721c672
eric <lee.eric.03@gmail.com>
2022-03-02 10:45:19 -0700

    functions

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,7 @@
+// Functions
+function describeCountry(country, population, capitalCity) {
+  console.log(`${country} has ${population} people and it's capital city is ${capitalCity}.`)
+}
+
+describeCountry('America', "350,000,000", 'Washington D.C.')
+
