2c541836e75aba88bab8e598d0509796a1a7c635
eric <lee.eric.03@gmail.com>
2022-03-02 11:12:09 -0700

    arrow funcs

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -5,3 +5,22 @@ function describeCountry(country, population, capitalCity) {
 
 describeCountry('America', "350,000,000", 'Washington D.C.')
 
+// Function declaration vs. Expressions
+
+// declaration
+function percentageOfWorld1(population) {
+  return (population/7900) * 100
+}
+
+const usPopulation = percentageOfWorld1(325)
+console.log(usPopulation)
+
+// expression
+const percentageOfWorld2 = function(population) {
+  return (population/7900) * 100
+}
+console.log(percentageOfWorld2(325))
+
+// Arrow functions
+const percentageOfWorld3 = population => (population/7900) * 100
+console.log(percentageOfWorld3(325))
