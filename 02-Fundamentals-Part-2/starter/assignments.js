0385a131ceaf586fb9f34833542e95b42391e8a1
eric <lee.eric.03@gmail.com>
2022-03-03 19:40:02 -0700

    functions2

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -9,7 +9,7 @@ describeCountry('America', "350,000,000", 'Washington D.C.')
 
 // declaration
 function percentageOfWorld1(population) {
-  return (population/7900) * 100
+  return Number(((population/7900) * 100).toFixed(0))
 }
 
 const usPopulation = percentageOfWorld1(325)
@@ -22,5 +22,13 @@ const percentageOfWorld2 = function(population) {
 console.log(percentageOfWorld2(325))
 
 // Arrow functions
-const percentageOfWorld3 = population => (population/7900) * 100
+const percentageOfWorld3 = population => {
+  return Number((population/7900) * 100).toFixed(0)
+}
 console.log(percentageOfWorld3(325))
+
+// Functions calling other functions
+function describePopulation(country, population) {
+  return `${country} has ${population} million people, which is about ${percentageOfWorld1(population)}% of the World.`
+}
+console.log(describePopulation("America", 325))
