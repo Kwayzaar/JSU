8de3b29bf0a214b933ede06ccfdfacad341fc5c6
eric <lee.eric.03@gmail.com>
2022-05-02 15:09:58 -0600

    sync

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xyzabc = 23;
+
+const calcAge = (birthyear) => 2037 - birthyear;
+
+console.log('hello');
+
+console.log(calcAge(1991));
