2718c5e4045fe25a7ee49ded02d28e19d3fe4d36
eric <lee.eric.03@gmail.com>
2022-05-17 10:04:03 -0600

    querySelectors

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent); //query selector grabs .message class, textContent shows only text of that element
+
+document.querySelector('.message').textContent = ' Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// read the value of a DOM element
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
