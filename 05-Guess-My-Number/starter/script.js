7dd28300944e794c02b711f695e32612048afcb9
eric <lee.eric.03@gmail.com>
2022-06-17 08:13:41 -0600

    style updates in dom

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent); //query selector grabs .message class, textContent shows only text of that element
 
+// Set the value of a DOM element
 document.querySelector('.message').textContent = ' Correct Number!';
 console.log(document.querySelector('.message').textContent);
 
@@ -11,3 +13,50 @@ document.querySelector('.score').textContent = 10;
 // read the value of a DOM element
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // If there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ' Correct Number!';
+
+    // Change bg on win
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    // Change input width on win
+    document.querySelector('.number').style.width = '30rem';
+
+    // If the guess is higher
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game :(';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //If the guess is lower
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game :(';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
