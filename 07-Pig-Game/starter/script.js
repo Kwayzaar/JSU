805f06596448303242ad58e39e8ff7154ac7ba50
Kwayzaar <lee.eric.03@gmail.com>
2022-07-07 10:22:17 -0600

    added selecting elements, starting conditions, beginning rolling dice functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+// Selecting Elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelecter('.btn--new');
+const btnRoll = document.querySelecter('.btn--roll');
+const btnHold = document.querySelecter('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classlist.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
