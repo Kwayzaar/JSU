c1bf7d8a92d3d7b02ad23860cf6f54faf2abd45e
Kwayzaar <lee.eric.03@gmail.com>
2022-07-07 11:08:21 -0600

    Update select elements list to inlcude current player score, Rolling dice handler function now adds rolls to score and checks for a 1 roll

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,6 +3,8 @@
 // Selecting Elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,12 +15,24 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// Rolling dice functionality
+let currentScore = 0;
+
+// Rolling dice handler function
 btnRoll.addEventListener('click', function () {
   // 1. Generate random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
   // 2. Display dice
-  diceEl.classlist.remove('hidden');
+  diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
 });
